8ed1d7394d4387e158512b205f66ebc23f547fe8
Eelco van Vliet <eelco@davelab.com>
2025-10-24 12:21:27 +0200

    added new script

---
+++ examples/defraction_box.py
@@ -0,0 +1,39 @@
+from pymeshup import Box
+
+grid_symmetry = False
+file_base = "defraction_box"
+if grid_symmetry:
+    file_base += "_half"
+
+length = 10
+width = 4
+height = 3
+drafts = [1, 2]
+
+box_buoy_filename = file_base + "_buoy.stl"
+box_base_filename = file_base + ".stl"
+
+half_width = width / 2
+half_length = length / 2
+
+# create the buoy mesh with the stern at x = 0
+box_buoy = Box(0, length, -half_width , half_width, 0, height)
+print(f"Saving buoyancy mesh {box_buoy_filename}")
+box_buoy.save(box_buoy_filename)
+
+# create the base mesh for the wave interaction with x = 0 in the centre
+box_base = box_buoy.move(x=-half_length)
+if grid_symmetry:
+    print("Grid symmetry on: cutting mesh at xz")
+    box_base = box_base.cut_at_xz()
+box_base_mesh = box_base.regrid(pct=3)
+print(f"Saving buoyancy mesh {box_base_filename}")
+box_base_mesh.save(box_base_filename)
+
+
+for draft in drafts:
+    box_draft = box_base.move(z=-draft).cut_at_waterline()
+    box_draft_mesh = box_draft.regrid(pct=3)
+    box_draft_filename = file_base + f"_{draft}m.stl"
+    print(f"Saving buoyancy mesh {box_draft_filename}")
+    box_draft_mesh.save(box_draft_filename)
